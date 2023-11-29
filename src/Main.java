5f848a14d06e7dd378b6513447cec1ceefce0df2
Programmer <mithunatom1@gmail.com>
2023-11-29 04:50:22 +0100

    Array_TASK 1.A.

--- src/Main.java
+++ src/Main.java
@@ -2,19 +2,39 @@
 public class Main {
     public static void main(String[] args) {
 
-                int[] numbersArray = new int[10];
+        int[] array = new int[]{10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
 
+        // 1.A
+        System.out.println("Original Array:");
+        printArray(array);
 
-        for (int i = 0; i < numbersArray.length; i++) {
-            numbersArray[i] = 9 - i;
+        for (int i = 0; i < array.length - 1; i++) {
+            if (array[i] > array[i + 1]) {
+                // Swap elements if the current element is greater than the next one
+                int temp = array[i];
+                array[i] = array[i + 1];
+                array[i + 1] = temp;
+            }
         }
 
 
-        System.out.println("Values in the array:");
+        System.out.println("\nArray After Swapping: (1.A)");
+        printArray(array);
+    }
 
-        for (int number : numbersArray) {
-            System.out.println(number);
+
+    private static void printArray(int[] arr) {
+        for (int value : arr) {
+            System.out.print(value + " ");
         }
+        System.out.println();
+
+
+
+
+
+
+
 
     }
 }
